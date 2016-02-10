1ac439dd9746c269b697ce70b6e40b506f3ac27a
Martín G. Morgado <geekblitz@geekblitz-imac.local>
2016-02-10 03:31:13 -0600

    emoji

--- Hola/ViewController.swift
+++ Hola/ViewController.swift
@@ -10,14 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var etiqueta: UILabel!
+    @IBOutlet weak var texto: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func saludar(sender: AnyObject) {
+        
+        let nombre : String = (texto.text)!
+        if nombre.isEmpty{
+            let alert = UIAlertController(title: "Error", message: "Introduce tu nombre en el cuadro de texto", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            
+            self.presentViewController (alert, animated: true, completion: nil)
+        }
+        else{
+            //nombre escrito
+            //etiqueta.text = "¡ Hola " + String(nombre) nombre escrito
+            etiqueta.text = "¡ Hola \(nombre) !"
+            texto.text = "🤓"
+            //modif 1 emoji
+        }
     }
 
 
